e9efb6ecb63f782559a7a90a31e0b7d2bfce059e
JeshuaBen <dev.09@pbhub.com.br>
2026-04-25 23:11:07 -0300

    feat(fonts): adding expo font, roboto type, and adjuts on layout to render the fonts  properly

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_600SemiBold,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 import '../global.css';
@@ -8,7 +17,26 @@ import { queryClient } from '@/shared/lib/react-query/query-client';
 import { colors } from '@/shared/theme';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_600SemiBold,
+    Roboto_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>

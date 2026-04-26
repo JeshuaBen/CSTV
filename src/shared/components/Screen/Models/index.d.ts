b5d18820939e609bd121d735c30b4a620d783bc2
JeshuaBen <dev.09@pbhub.com.br>
2026-04-26 11:31:38 -0300

    feat(arch): removing mvvm structure from shared components

--- src/shared/components/Screen/Models/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-
-export type ScreenProps = {
-  children: ReactNode;
-  className?: string;
-};
--- src/shared/components/Teams/Models/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type TeamProps = {
-  team1?: string;
-  team2?: string;
-  imageTeam1?: string;
-  imageTeam2?: string;
-};
